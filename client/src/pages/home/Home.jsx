6081226ac48194300c7b2a8c9cfc7a90f65b4cf6
kirstinmegga.ramos@gmail.com <kirstinmegga.ramos@gmail.com>
2023-11-28 18:20:25 -0500

    test

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -14,10 +14,10 @@ const Home = () => {
             <Header />
             <div className="homeContainer">
                 <Featured />
-                <h1 className="homeTitle">Browse by property type</h1>
-                <PropertyList />
-                <h1 className="homeTitle">Homes guests love</h1>
-                <FeaturedProperties/>
+                {/* <h1 className="homeTitle">Browse by property type</h1> */}
+                {/* <PropertyList /> */}
+                {/* <h1 className="homeTitle">Homes guests love</h1>
+                <FeaturedProperties/> */}
                 <MailList/>
                 <Footer/>
             </div>
