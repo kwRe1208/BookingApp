ab641f8e600f445e5183395bd08a945574d9213a
kwRe1208 <145515829+kwRe1208@users.noreply.github.com>
2023-12-12 18:41:09 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -45,7 +45,9 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.listen(8800, () => {
+const port = process.env.PORT || 8800;
+
+app.listen(port, () => {
     connect();
-    console.log("Backend server is running!");
+    console.log(`Backend server is running on port ${port}`);
 });
